9fef25904f344027953f3c021c9dfe6fd085375b
fabio-guilherme <fabio.guilherme@gmail.com>
2023-12-29 02:17:39 +0000

    Incluir Alunos

--- main.py
+++ main.py
@@ -1,27 +1,22 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow, QTableWidgetItem
+from PyQt6.QtWidgets import QApplication, QMainWindow, QTableWidgetItem, QDialog
 from PyQt6.uic import loadUi
 import mysql.connector
 
 class AlunoApp(QMainWindow):
     def __init__(self):
         super().__init__()
-        loadUi("alunos.ui", self)
+        loadUi("ui/alunos.ui", self)
         self.initUI()
 
     def initUI(self):
         self.btnConsultar.clicked.connect(self.consultar_alunos)
+        self.btnNovoAluno.clicked.connect(self.abrir_janela_novo_aluno)
 
     def consultar_alunos(self):
         # Configurar a conexão com o banco de dados
         try:
-            connection = mysql.connector.connect(
-                host="localhost",
-                user="root",
-                password="password",
-                database="centroexplicacoes2"
-            )
-
+            connection = conecta()
             cursor = connection.cursor()
 
             # Consulta para obter todos os alunos
@@ -46,6 +41,56 @@ class AlunoApp(QMainWindow):
                 cursor.close()
                 connection.close()
 
+    def abrir_janela_novo_aluno(self):
+        # Cria uma instância da janela para incluir um novo aluno
+        nova_janela = IncluirAlunoWindow()
+        nova_janela.exec()
+
+class IncluirAlunoWindow(QDialog):
+    def __init__(self):
+        super().__init__()
+        loadUi("ui/incluir_aluno.ui", self)
+        self.initUI()
+
+    def initUI(self):
+        self.btnSalvar.clicked.connect(self.incluir_novo_aluno)
+
+    def incluir_novo_aluno(self):
+        # Obter os dados do formulário
+        nome = self.lineEditNome.text()
+        contato = self.lineEditContato.text()
+
+        # Lógica para incluir um novo aluno no banco de dados
+        try:
+            connection = conecta()
+            cursor = connection.cursor()
+
+            # Inserir um novo aluno
+            query = "INSERT INTO aluno (Nome, Contato) VALUES (%s, %s)"
+            values = (nome, contato)
+            cursor.execute(query, values)
+
+            # Commit da transação
+            connection.commit()
+
+            # Fechar a janela após a inclusão
+            self.accept()
+
+        except Exception as e:
+            print("Erro ao incluir aluno:", e)
+
+        finally:
+            if connection.is_connected():
+                cursor.close()
+                connection.close()
+
+def conecta():
+    return mysql.connector.connect(
+                host="localhost",
+                user="root",
+                password="password",
+                database="centroexplicacoes2"
+            )
 def main():
     app = QApplication(sys.argv)
     window = AlunoApp()
